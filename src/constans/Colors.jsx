276a659b86367ae7f0218739d6eb7e4a73f41d05
Tamira2804 <tamira2804@gmail.com>
2023-12-22 13:08:22 +0200

    add fonts and colors

---
+++ src/constans/Colors.jsx
@@ -0,0 +1,18 @@
+const Colors = {
+  primaryBg: "#ffffff",
+
+  primaryText: "#212121",
+  secondaryText: "rgba(33,33,33,0.8)",
+  linkedText: "#1B4371",
+
+  inputBg: "#F6F6F6",
+  inputBorder: "#e8e8e8",
+
+  commentBg: "rgba(0, 0, 0, 0.03)",
+
+  white: "#ffffff",
+  gray: "#BDBDBD",
+  orange: "#ff6c00",
+};
+
+export default Colors;
